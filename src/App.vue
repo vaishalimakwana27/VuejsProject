f73a2261719aae8945efc5eb3f7407624aea23da
“Vaishali <vaishali.m@amarinfotech.com>
2023-05-08 18:39:57 +0530

    latest changes uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,104 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div className="parent-div">
+    <mainheader />
+    <h1>Vue JS Training...!!!!!!!!</h1>
+    <h3>{{ userName }}</h3>
+    <h3>Email ID:- {{ userEmail }}</h3>
+    <h3>User ID:- {{ userId }}</h3>
+    <h3>Total Count:- {{ totalCount }}</h3>
+    <button class="add-btn" @click="onShowUser()">Click me</button>
+    <button class="alert-button" @click="onShowAlert()">Alert</button>
+    <div>
+      <p class="show-data">{{ dataList }}</p>
+      <button class="show-data-button" @click="onShowData()">Show Data</button>
     </div>
-  </header>
-
-  <main>
-   <h1>Hello , Have Nice Day......</h1>
-  </main>
+    <div>
+      <p>{{ filterData }}</p>
+      <button class="show--filter-data-button" @click="showFilterData()">Filter Data</button>
+    </div>
+    <footermain />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import mainheader from "./components/Header.vue"; 
+import footermain from "./components/Footer.vue";
+export default {
+  name:"App",
+  components: { mainheader , footermain} ,
+  props:{},
+  data(){
+    return {
+      userName:"Vaishali Makwana",
+      userEmail:"vaishali@gmail.com",
+      userId:"",
+      totalCount:0, 
+      obj:[
+        { name:"Juhi", age:26 },
+        { name:"Mahi", age:21 },
+        { name:"Test", age:34 },
+        { name:"Vaishali", age:22 },
+      ],
+      dataList:[], 
+      filterData:[],
+    }
+  },
+  methods:{
+    onShowUser(){
+      console.log("this",this);
+      this.userId = 12345;
+      this.totalCount = 220;
+    },
+    onShowAlert(){
+      alert("Alert Page......");
+    },
+    onShowData(){
+      const newArray = this.obj.map((ele, index, arr) => {
+        return ele.name;
+      });
+      this.dataList = newArray;
+      console.log("newArray", newArray);
+    },
+    showFilterData(){
+      const filterDataArray = this.obj.filter((maxAge, index, arr) => {
+        return maxAge.age > 22;
+    });
+    this.filterData = filterDataArray;
+    console.log("newFilterData", filterDataArray)
+
+    }
+  }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+<style>
+.add-btn{
+  background-color: teal;
+  color:white;
+  border: 3px solid black;
+  margin-right: 30px;
+  font-size: 25px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.alert-button{
+  background-color: blueviolet;
+  color: white;
+  border: 3px solid black;
+  font-size: 25px;
+  margin-right: 30px;
+}
+.show-data-button{
+  background-color: teal;
+  color: white;
+  border: 3px solid black;
+  font-size: 25px;
+  margin-right: 30px;
+}
+.show--filter-data-button{
+  background-color: blueviolet;
+  color: white;
+  border: 3px solid black;
+  font-size: 25px;
+  margin-right: 30px;
 }
 </style>
+
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{ footerName }}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"Footer",
+    components:{},
+    data(){
+        return{
+            footerName:"@Copyright 2023. All Rights Reserved."
+        }
+    }
+}
+</script>
